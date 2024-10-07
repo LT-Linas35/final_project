77a82426b6b719f8e31d14ff00fa5bfbf412ad5c
Linas M. <lincasm@gmail.com>
2024-10-07 17:54:41 +0300

    Updating Terraform

--- terraform/scripts/nginx.sh
+++ terraform/scripts/nginx.sh
@@ -14,24 +14,24 @@ chown ec2-user:ec2-user /home/ec2-user/.ssh/id_rsa
 chmod 600 /home/ec2-user/.ssh/id_rsa
 
 # Install Puppet, Nginx, and Nano text editor
-rpm -Uvh https://yum.puppet.com/puppet8-release-el-9.noarch.rpm
-dnf -y install nano puppet-agent nginx
+#rpm -Uvh https://yum.puppet.com/puppet8-release-el-9.noarch.rpm
+dnf -y install nano nginx #puppet-agent
 
 # Set Puppet server hostname
-export pupethost=$(hostname)
+#export pupethost=$(hostname)
 
 # Configure Puppet agent settings
-cat <<EOF > /etc/puppetlabs/puppet/puppet.conf
-[main]
-certname = $pupethost
-server = ${controller_hostname}
-EOF
+#cat <<EOF > /etc/puppetlabs/puppet/puppet.conf
+#[main]
+#certname = $pupethost
+#server = ${controller_hostname}
+#EOF
 
 # Bootstrap Puppet SSL certificates to establish trust with the Puppet server
-/opt/puppetlabs/bin/puppet ssl bootstrap
+#/opt/puppetlabs/bin/puppet ssl bootstrap
 
 # Enable Puppet service to run on system startup
-systemctl enable puppet
+#systemctl enable puppet
 
 # Reboot the system to apply all changes
 systemctl reboot
