e866a078dd0c0b98df13fd0fc518a226184f77a8
Linas M. <126604774+LT-Linas35@users.noreply.github.com>
2024-11-02 23:58:09 +0200

    Update roles and policys.tf

--- terraform/roles and policys.tf
+++ terraform/roles and policys.tf
@@ -59,9 +59,11 @@ resource "aws_iam_user_policy" "s3_user_policy" {
           "s3:PutObject",
           "s3:DeleteObject"
         ]
-        Resource = "*"
+        Resource = [
+          "arn:aws:s3:::${var.nextcloud_install.S3_BUCKET}",   
+          "arn:aws:s3:::${var.nextcloud_install.S3_BUCKET}/*"
+        ]
       }
     ]
   })
 }
-
